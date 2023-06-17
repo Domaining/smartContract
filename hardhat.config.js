60c440c62377e669e664ddf15dfac560110e4afa
innookeke <inno.okeke@outlook.com>
2023-06-17 16:06:06 +0100

    update file

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     mumbai: {
       url: "https://rpc-mumbai.maticvigil.com/",
-      accounts: ["e9efaaea6e204db833c16f1a1f8ec33161c5cd3d1c94b2b97c976bb44d75149c"],
+      accounts: [""],
     }
   }
 };
