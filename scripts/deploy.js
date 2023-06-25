20f2e2aa35e809c241403855156bddd79a7046db
innookeke <inno.okeke@outlook.com>
2023-06-25 22:32:38 +0100

    update file

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,18 +1,17 @@
 const main = async () => {
     const domainContractFactory = await hre.ethers.getContractFactory('Domains');
-    const domainContract = await domainContractFactory.deploy("core");
+    const domainContract = await domainContractFactory.deploy("ftm");
     await domainContract.deployed();
   
     console.log("Contract deployed to:", domainContract.address);
   
-    // CHANGE THIS DOMAIN TO SOMETHING ELSE! I don't want to see OpenSea full of bananas lol
     let txn = await domainContract.register("leprof",  {value: hre.ethers.utils.parseEther('0.1')});
     await txn.wait();
     console.log("Minted domain leprof.core");
   
     txn = await domainContract.setRecord("leprof", "Am I a web3 dev??");
     await txn.wait();
-    console.log("Set record for leprof.core");
+    console.log("Set record for leprof.ftm");
   
     const address = await domainContract.getAddress("leprof");
     console.log("Owner of domain leprof:", address);
